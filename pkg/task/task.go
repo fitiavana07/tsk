c37f9002722b1455d75e4bc096a40389c6fec649
Fitiavana Ramanandafy <fitiavana.ramanandafy@gmail.com>
2020-08-26 20:10:47 +0300

    task

--- pkg/task/task.go
+++ pkg/task/task.go
@@ -2,19 +2,14 @@ package task
 
 // Task is something to do, moved into doing, then marked done
 type Task struct {
-	id    int
-	name  string
+	ID    int
+	Name  string
 	state State
 }
 
 // New constructs a new task
-func New() *Task {
-	return &Task{}
-}
-
-// ID returns the id of a task
-func (t *Task) ID() int {
-	return t.id
+func New(name string) *Task {
+	return &Task{Name: name}
 }
 
 // State is the state a task is in
--- pkg/task/task_test.go
+++ pkg/task/task_test.go
@@ -1,5 +1,9 @@
 package task
 
+import (
+	"testing"
+)
+
 /*
 Testing of domain of the application.
 
@@ -13,3 +17,21 @@ A Task has these attributes:
 	- a date
 	- a context
 */
+
+func TestNew(t *testing.T) {
+	taskName := "do the task"
+	task := New(taskName)
+
+	t.Run("CheckId", func(t *testing.T) {
+		got := task.ID
+		if got != 0 {
+			t.Errorf("got task.ID=%d, want 0", got)
+		}
+	})
+	t.Run("CheckName", func(t *testing.T) {
+		got := task.Name
+		if got != taskName {
+			t.Errorf("got task.Name=%q, want=%q", got, taskName)
+		}
+	})
+}
