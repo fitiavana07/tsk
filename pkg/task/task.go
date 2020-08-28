a0ae199e76b73be29482e359b93cd3f388e3d512
Fitiavana Ramanandafy <fitiavana.ramanandafy@gmail.com>
2020-08-28 08:08:12 +0300

    string() for task and state

--- pkg/task/task.go
+++ pkg/task/task.go
@@ -1,10 +1,14 @@
 package task
 
+import (
+	"fmt"
+)
+
 // Task is something to do, moved into doing, then marked done
 type Task struct {
 	ID    int
 	Name  string
-	state State
+	State State
 }
 
 // New constructs a new task
@@ -12,8 +16,30 @@ func New(id int, name string) *Task {
 	return &Task{ID: id, Name: name}
 }
 
+func (t Task) String() string {
+	return fmt.Sprintf(
+		"Task(ID=%d, Name=%q, State=%s)",
+		t.ID,
+		t.Name,
+		t.State,
+	)
+}
+
 // State is the state a task is in
 type State uint8
+
+func (st State) String() string {
+	switch st {
+	case StateTodo:
+		return "Todo"
+	case StateDoing:
+		return "Doing"
+	case StateDone:
+		return "Done"
+	default:
+		return ""
+	}
+}
 
 // Possible states of tasks
 const (
--- pkg/task/task_test.go
+++ pkg/task/task_test.go
@@ -36,3 +36,34 @@ func TestNew(t *testing.T) {
 		}
 	})
 }
+
+func TestTaskString(t *testing.T) {
+	tsk := New(20, "do my task")
+	got := tsk.String()
+	want := "Task(ID=20, Name=\"do my task\", State=Todo)"
+	if got != want {
+		t.Errorf("got: %s, want: %s", got, want)
+	}
+}
+
+func TestStateString(t *testing.T) {
+	var got, want string
+
+	got = StateTodo.String()
+	want = "Todo"
+	if got != want {
+		t.Errorf("got: %s, want: %s", got, want)
+	}
+
+	got = StateDoing.String()
+	want = "Doing"
+	if got != want {
+		t.Errorf("got: %s, want: %s", got, want)
+	}
+
+	got = StateDone.String()
+	want = "Done"
+	if got != want {
+		t.Errorf("got: %s, want: %s", got, want)
+	}
+}
